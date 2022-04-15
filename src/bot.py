b2c58d9a4ab816e268ae2c400a2d2e3f3b5b5cf4
Simon Earman <earmansimon@gmail.com>
2022-04-16 01:51:34 +0200

    feat: say and rng commands

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,7 @@
 import discord
 from dotenv import load_dotenv
 from os import getenv
+from random import randint
 
 client = discord.Client()
 load_dotenv()
@@ -19,5 +20,15 @@ async def on_message(message):
     command = args.pop(0).lower()
     if command == "ping":
         await message.channel.send("pong")
+    elif command == "say":
+        await message.delete()
+        await message.channel.send(" ".join(args))
+    elif command == "rng":
+        if len(args) == 1:
+            rng_min = 1
+        else:
+            rng_min = int(args.pop(0))
+        rng_max = int(args.pop(0))
+        await message.channel.send(f"Losowa liczba od {rng_min} do {rng_max}: **{randint(rng_min, rng_max)}**")
 
 client.run(getenv("TOKEN"))
